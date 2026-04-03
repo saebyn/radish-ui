8dc4fa95c6219b775f5942d1a00b3e40e8289ba5
saebyn <me@saebyn.dev>
2026-04-02 21:17:46 -0700

    feat: add vite-env.d.ts for Vite client type references and adjust CSS import comment

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// oxlint-disable-next-line no-unassigned-import -- CSS side-effect import, intentional
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
